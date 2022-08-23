950df731751325abff0da0c6d2faca8e045d4dec
Beniamin Cosma <cosma.beny@gmail.com>
2022-08-23 03:36:28 +0300

    Update npm packages

--- apps/delegation-api/src/common/helpers/query-response.helper.ts
+++ apps/delegation-api/src/common/helpers/query-response.helper.ts
@@ -1,8 +1,5 @@
 import { QueryResponse } from '@elrondnetwork/erdjs/out/smartcontracts';
 
-/**
- * This class will contain helper (static) methods related to @erdjs/QueryResponse
- */
 export class QueryResponseHelper {
   static handleQueryAmountResponse(response: QueryResponse): string {
     return response?.returnData[0]?.asBigInt.toFixed();
