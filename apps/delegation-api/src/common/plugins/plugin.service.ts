79c690ecc8429ba40a199c91d08a4f6e1bc0918a
Beniamin Cosma <cosma.beny@gmail.com>
2022-08-24 11:24:07 +0300

    added eslint disable for unused vars in plugins service

--- apps/delegation-api/src/common/plugins/plugin.service.ts
+++ apps/delegation-api/src/common/plugins/plugin.service.ts
@@ -3,8 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PluginService {
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onProviderFeeChanged(contractAddress: string, args: string[]): Promise<void> { return; }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onProviderTotalDelegationCapServiceChanged(contractAddress: string, from: string, args: string[]): Promise<void> { return; }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onDelegate(senderAddress: string, contractAddress: string): Promise<void> { return; }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onUnDelegate(senderAddress: string, contractAddress: string): Promise<void> { return; }
 }
