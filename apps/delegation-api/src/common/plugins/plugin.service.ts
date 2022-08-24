12b53a5218ac41c325b40a12ad02f4040d6e8440
Beniamin Cosma <cosma.beny@gmail.com>
2022-08-24 11:16:20 +0300

    Use `return` instead of disabling eslint

--- apps/delegation-api/src/common/plugins/plugin.service.ts
+++ apps/delegation-api/src/common/plugins/plugin.service.ts
@@ -2,12 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PluginService {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async onProviderFeeChanged(contractAddress: string, args: string[]): Promise<void> { }
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async onProviderTotalDelegationCapServiceChanged(contractAddress: string, from: string, args: string[]): Promise<void> { }
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async onDelegate(senderAddress: string, contractAddress: string): Promise<void> { }
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async onUnDelegate(senderAddress: string, contractAddress: string): Promise<void> { }
+
+  onProviderFeeChanged(contractAddress: string, args: string[]): Promise<void> { return; }
+  onProviderTotalDelegationCapServiceChanged(contractAddress: string, from: string, args: string[]): Promise<void> { return; }
+  onDelegate(senderAddress: string, contractAddress: string): Promise<void> { return; }
+  onUnDelegate(senderAddress: string, contractAddress: string): Promise<void> { return; }
 }
