24ceeb5540f943910643fcf3620e1f6a759a31c0
Beniamin Cosma <cosma.beny@gmail.com>
2022-08-24 10:40:49 +0300

    Remove abstract class. Ignore eslint rules for PluginsService

--- apps/delegation-api/src/common/plugins/plugin.service.ts
+++ apps/delegation-api/src/common/plugins/plugin.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export abstract class PluginService {
-  abstract onProviderFeeChanged(contractAddress: string, args: string[]): Promise<void>;
-  abstract onProviderTotalDelegationCapServiceChanged(contractAddress: string, from: string, args: string[]): Promise<void>;
-  abstract onDelegate(senderAddress: string, contractAddress: string): Promise<void>;
-  abstract onUnDelegate(senderAddress: string, contractAddress: string): Promise<void>;
+export class PluginService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async onProviderFeeChanged(contractAddress: string, args: string[]): Promise<void> { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async onProviderTotalDelegationCapServiceChanged(contractAddress: string, from: string, args: string[]): Promise<void> { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async onDelegate(senderAddress: string, contractAddress: string): Promise<void> { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async onUnDelegate(senderAddress: string, contractAddress: string): Promise<void> { }
 }
