22d3b4d9c0464496493c0d639fed3e6e43a7a27c
AlexandruPislariu <alexandru.pislariu@multiversx.com>
2024-02-01 14:25:04 +0200

    Update VerifyIdentity Loader

--- apps/delegation-api/src/common/services/provider-manager/verify-identity/loader/verify-identity-loader.module.ts
+++ apps/delegation-api/src/common/services/provider-manager/verify-identity/loader/verify-identity-loader.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { CacheManagerModule } from "../../../cache-manager/cache-manager.module";
-import { GithubModule } from "../../profile/github/github.module";
 import { VerifyIdentityLoaderService } from "./verify-identity-loader.service";
+import { AssetsModule } from "../../../assets/assets.module";
 
 @Module({
   imports: [
     CacheManagerModule,
-    GithubModule,
+    AssetsModule,
   ],
   providers: [
     VerifyIdentityLoaderService,
