e0a4a7d617c266a773c69bdbf0451d6774922329
AlexandruPislariu <alexandru.pislariu@multiversx.com>
2024-05-21 17:24:35 +0300

    Add undefined checks for ContractConfig

--- apps/delegation-api/src/modules/delegation/dto/contract-config.dto.ts
+++ apps/delegation-api/src/modules/delegation/dto/contract-config.dto.ts
@@ -20,12 +20,12 @@ export class ContractConfigResponseDto {
     response.serviceFee = contractConfig[1]?.asFixed();
     response.maxDelegationCap = contractConfig[2]?.asFixed();
     response.initialOwnerFunds = contractConfig[3]?.asFixed();
-    response.automaticActivation = contractConfig[4].asBool();
-    response.withDelegationCap = contractConfig[5].asBool();
-    response.changeableServiceFee = contractConfig[6].asBool();
-    response.checkCapOnRedelegate = contractConfig[7].asBool();
-    response.createdNonce = contractConfig[8].asNumber();
-    response.unBondPeriod = contractConfig[9].asNumber();
+    response.automaticActivation = contractConfig[4]?.asBool();
+    response.withDelegationCap = contractConfig[5]?.asBool();
+    response.changeableServiceFee = contractConfig[6]?.asBool();
+    response.checkCapOnRedelegate = contractConfig[7]?.asBool();
+    response.createdNonce = contractConfig[8]?.asNumber();
+    response.unBondPeriod = contractConfig[9]?.asNumber();
 
     return response;
   }
